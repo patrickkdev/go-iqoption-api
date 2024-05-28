2bc84a3b9b33193ade4f18bff5627e892f9da35a
Patrick Ferreira <115599753+patrickkdev@users.noreply.github.com>
2024-05-28 11:28:48 -0300

    Update auth.go

--- broker/auth.go
+++ broker/auth.go
@@ -91,3 +91,32 @@ func (c *Client) ConnectSocket() error {
 
 	return nil
 }
+
+func (c *Client) authenticate() (*types.AuthenticationResponse, error) {
+	requestEvent := requestEvent{
+		Name: "authenticate",
+		Msg: map[string]interface{}{
+			"ssid":              c.session.SSID,
+			"protocol":          3,
+			"client_session_id": "",
+			"session_id":        "",
+		},
+	}.WithRandomRequestId()
+
+	resp, err := c.ws.EmitWithResponse(requestEvent, "authenticated", c.getTimeout())
+	if err != nil {
+		return nil, err
+	}
+
+	responseEvent, err := tjson.Unmarshal[types.AuthenticationResponse](resp)
+	if err != nil {
+		return nil, err
+	}
+
+	if !responseEvent.Msg {
+		return nil, fmt.Errorf("authentication failed")
+	}
+
+	return &responseEvent, nil
+}
+
