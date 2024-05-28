a71789b59d292d8e0ae3462140a166a1808cc953
Patrick Ferreira <115599753+patrickkdev@users.noreply.github.com>
2024-05-28 11:27:53 -0300

    Update auth.go

--- broker/auth.go
+++ broker/auth.go
@@ -3,10 +3,8 @@ package broker
 import (
 	"encoding/json"
 	"fmt"
-	"time"
 
 	"github.com/patrickkdev/Go-IQOption-API/internal/brokerws"
-	"github.com/patrickkdev/Go-IQOption-API/internal/debug"
 )
 
 type LoginData struct {
@@ -51,44 +49,20 @@ func Logout(url string, session *Session) error {
 // Connect to broker websocket and authenticates
 // Is always trying to reconnect if connection is lost
 // Automatically subscribes to default events like 'balance-changed', 'trade-changed', 'timesync' and 'position-changed'
-func (c *Client) ConnectSocket(connRetryCount ...int) error {
+func (c *Client) ConnectSocket() error {
 	maxRetryCount := 10
 	retryCount := 0
-
-	if len(connRetryCount) > 0 {
-		retryCount = connRetryCount[0]
-		fmt.Printf("Reconnecting user %s... (%d/%d)\n", c.session.LoginData.Email, retryCount, 10)
-	}
 
 	if retryCount >= maxRetryCount {
 		return fmt.Errorf("max retry count reached for user %s", c.session.LoginData.Email)
 	}
 
-	// Try to persist connection
-	reconnect := func() {
-		// REFACTOR NEEDED
-		
-		return // MAKE NEXT CODE UNREACHABLE IN ORDER TO TEST FUNCIONALITY WITHOUT RECONNECT 
-		
-		// WE SUSPECT THAT THIS RECONNECT FUNCTION IS WHAT IS CAUSING THE SERVER TO SHUT DOWN
-		time.Sleep(time.Minute)
-
-		err := c.ConnectSocket(retryCount + 1)
-		if err != nil {
-			debug.IfVerbose.Println("Reconnect error", err.Error())
-		}
-	}
-
-	newSocketConn, err := brokerws.NewSocketConnection(c.brokerEndpoints.WSAPIURL, c.defaultTimeoutDuration, reconnect)
+	newSocketConn, err := brokerws.NewSocketConnection(c.brokerEndpoints.WSAPIURL, c.defaultTimeoutDuration)
 	if err != nil {
 		return err
 	}
 
 	c.ws = newSocketConn
-
-	// REFACTOR NEEDED
-	// It seems like answering heartbeats is not needed, so we remove it
-	c.startAnsweringHeartBeats()
 
 	c.keepServerTimestampUpdated()
 
