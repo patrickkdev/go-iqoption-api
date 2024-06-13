57f82280f45e1f21be7a9d0513832869ee0f813c
Patrick Ferreira <115599753+patrickkdev@users.noreply.github.com>
2024-06-13 10:02:53 -0300

    Update balances.go

--- internal/types/balances.go
+++ internal/types/balances.go
@@ -14,13 +14,13 @@ type BalanceUpdate struct {
 	ID                  int     `json:"id"`
 	Amount              float64 `json:"amount"`
 	EnrolledAmount      float64 `json:"enrolled_amount"`
-	BonusAmount         int     `json:"bonus_amount"`
-	BonusEnrolledAmount int     `json:"bonus_enrolled_amount"`
+	BonusAmount         float64 `json:"bonus_amount"`
+	BonusEnrolledAmount float64 `json:"bonus_enrolled_amount"`
 	Currency            string  `json:"currency"`
 	Type                int     `json:"type"`
 	Index               int64   `json:"index"`
 	IsFiat              bool    `json:"is_fiat"`
 	NewAmount           float64 `json:"new_amount"`
-	BonusTotalAmount    int     `json:"bonus_total_amount"`
+	BonusTotalAmount    float64 `json:"bonus_total_amount"`
 	IsMarginal          bool    `json:"is_marginal"`
 }
